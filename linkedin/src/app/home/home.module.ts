82e4fdbffb0fb6813327709c4293cae9082066b1
Josh Kersey <63331369+pulanski@users.noreply.github.com>
2022-03-08 17:43:43 -0500

    feat: 🎸 add main feature components of app
    add main feature components of app to overall architecture and begin
    styling the backdrop for the profile summary component

--- linkedin/src/app/home/home.module.ts
+++ linkedin/src/app/home/home.module.ts
@@ -6,10 +6,12 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 import { HeaderComponent } from './components/header/header.component';
-import { PopoverComponent } from './components/header/popover/popover.component';
+import { ProfileSummaryComponent } from './components/profile-summary/profile-summary.component';
+import { StartPostComponent } from './components/start-post/start-post.component';
+import { AdvertisingComponent } from './components/advertising/advertising.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
-  declarations: [HomePage, HeaderComponent, PopoverComponent],
+  declarations: [HomePage, HeaderComponent, ProfileSummaryComponent, StartPostComponent, AdvertisingComponent],
 })
 export class HomePageModule {}
