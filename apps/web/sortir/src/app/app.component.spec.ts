f2722499fc317aa74f322f38acdba3f562c4ef6e
Josh Kersey <63331369+pulanski@users.noreply.github.com>
2022-03-31 15:19:15 -0400

    refactor: 💡 remove ionic dependencies from web apps (#32)
    remove ionic dependencies from web apps, migrating to exclusively
    building via angular

--- apps/web/sortir/src/app/app.component.spec.ts
+++ apps/web/sortir/src/app/app.component.spec.ts
@@ -1,20 +1,28 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TestBed, waitForAsync } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
 describe('AppComponent', () => {
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    }).compileComponents();
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({ declarations: [AppComponent, NxWelcomeComponent] }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-  // TODO: add more tests!
+
+  it(`should have as title 'web-sortir'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('web-sortir');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome web-sortir');
+  });
 });
