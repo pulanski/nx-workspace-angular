3e7c1ed04972255c1e1ba34c9fb4b8ad77480a7a
Josh Kersey <63331369+pulanski@users.noreply.github.com>
2022-04-01 22:13:13 -0400

    test: 💍 determine whether github actions plugins are working (#63)
    test whether todos github action or ranger are working

--- libs/api/core/feature/src/lib/api-core-feature.module.ts
+++ libs/api/core/feature/src/lib/api-core-feature.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  // @todo: test todos
+  // @body: test that both the todo plugin is activated as well as the ranger plugin
   imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot()],
   controllers: [],
   providers: [],
