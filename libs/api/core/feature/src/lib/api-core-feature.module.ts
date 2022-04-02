d04b902f8aa99d225e404bbdda17a4a187ecb418
pulanski <joshuakersey50@gmail.com>
2022-04-01 23:46:13 -0400

    ci: 🎡 add starter node workflow
    add node ci workflow to ensure building and testing are passing

--- libs/api/core/feature/src/lib/api-core-feature.module.ts
+++ libs/api/core/feature/src/lib/api-core-feature.module.ts
@@ -3,8 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  // TODO add imports
-  // testing to see if the todo github action is configured correctly
   imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot()],
   controllers: [],
   providers: [],
