95ba47f3d0a912ae4970c7dfd2637b4fb7c64459
pulanski <joshuakersey50@gmail.com>
2022-04-03 21:19:46 -0400

    feat: 🎸 scaffold out library for shared data-access
    scaffold out library for shared data-access, typically will include
    models shared between backend and frontend

--- libs/shared/data-access/models/jest.config.js
+++ libs/shared/data-access/models/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'shared-utils-compodoc-tools',
+  displayName: 'shared-data-access-models',
   preset: '../../../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../coverage/libs/shared/utils/compodoc-tools',
+  coverageDirectory: '../../../../coverage/libs/shared/data-access/models',
 };
