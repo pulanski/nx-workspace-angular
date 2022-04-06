2b8531c9858af67fee41a2a509b20728997e16b4
pulanski <joshuakersey50@gmail.com>
2022-04-06 00:06:27 -0400

    perf: ⚡️ change start-post component change detection strategy
    change start-post component change detection strategy from default to
    OnPush

--- libs/web/home/ui/start-post/src/lib/start-post.component.ts
+++ libs/web/home/ui/start-post/src/lib/start-post.component.ts
@@ -1,12 +1,13 @@
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { NbWindowService } from '@nebular/theme';
 
 @Component({
   selector: 'nx-workspace-start-post',
   templateUrl: './start-post.component.html',
   styleUrls: ['./start-post.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StartPostComponent implements OnInit {
   @ViewChild('startPostWindowTemplate', { read: TemplateRef })
